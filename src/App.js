998b2c6fc426d47c91d8e29d970abb16ccc2c67c
Ryan Walker <ryandanielwalker@gmail.com>
2021-07-16 14:09:02 -0700

    Add header with links to homepage and signIn page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import SignIn from "./../src/components/SignIn";
 import RestaurantControl from './components/RestaurantControl'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from './components/Header'
 
 function App() {
   return (
     <Router>
-      {/* I CURRENTLY HAVE NO HEADER */}
+      <Header />
       <Switch>
         <Route path="/signin">
           <SignIn />
