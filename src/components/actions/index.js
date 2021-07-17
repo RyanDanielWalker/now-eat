cb30f8a908241a3ee11364a57914bb693ca2ecab
Ryan Walker <ryandanielwalker@gmail.com>
2021-07-16 19:52:06 -0700

    Install axios, write axios function... Still CORS

--- src/components/actions/index.js
+++ src/components/actions/index.js
@@ -1,4 +1,5 @@
 import * as c from './ActionTypes';
+import axios from 'axios';
 
 export const requestRestaurants = () => ({
   type: c.REQUEST_RESTAURANTS
@@ -32,22 +33,46 @@ export const nullSelectedRestaurant = () => {
   }
 }
 
+// export const makeApiCall = () => {
+//   return dispatch => {
+//     dispatch(requestRestaurants);
+//     return fetch(`https://api.yelp.com/v3/businesses/search?location=portland`, {
+//       method: 'GET',
+//       mode: "no-cors",
+//       headers: {
+//         Authorization: 'Bearer ' + process.env.REACT_APP_YELP_API_KEY,
+//         'Content-type': 'application/json'
+//       }
+//     })
+//       .then(response => response.json())
+//       .then(
+//         (jsonifiedResponse) => {
+//           dispatch(getRestaurantsSuccess(jsonifiedResponse.results));
+//         })
+//       .catch((error) => {
+//         dispatch(getRestaurantsFailure(error));
+//       });
+//   }
+// }
+
 export const makeApiCall = () => {
   return dispatch => {
     dispatch(requestRestaurants);
-    return fetch(`https://api.yelp.com/v3/businesses/search?location=portland`, {
-      method: 'GET',
+    let yelpREST = axios.create({
+      baseURL: "https://api.yelp.com/v3/",
       mode: "no-cors",
+      method: "GET",
       headers: {
-        Authorization: 'Bearer ' + process.env.REACT_APP_YELP_API_KEY,
-        'Content-type': 'application/json'
+        Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`,
+        "Content-type": "application/json",
+        // "Access-Control-Allow-Origin": "http:localhost:3000"
       }
     })
+    return yelpREST('location', { params: { city: 'portland' } })
       .then(response => response.json())
-      .then(
-        (jsonifiedResponse) => {
-          dispatch(getRestaurantsSuccess(jsonifiedResponse.results));
-        })
+      .then((jsonifiedResponse) => {
+        dispatch(getRestaurantsSuccess(jsonifiedResponse.results));
+      })
       .catch((error) => {
         dispatch(getRestaurantsFailure(error));
       });
