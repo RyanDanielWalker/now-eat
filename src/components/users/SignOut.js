53108573ba300e701083b0d23aba939c3accaf68
Ryan Walker <ryandanielwalker@gmail.com>
2021-07-25 16:45:10 -0700

    Successfully update friend properties to null in database upon signing out

--- src/components/users/SignOut.js
+++ src/components/users/SignOut.js
@@ -3,24 +3,44 @@ import { useDispatch } from 'react-redux';
 import firebase from 'firebase/app';
 import { useHistory } from 'react-router-dom';
 import * as a from './../../actions'
-import { withFirestore } from 'react-redux-firebase';
+import { withFirestore, useFirestoreConnect, useFirestore } from 'react-redux-firebase';
 
 const SignOut = () => {
 
   const history = useHistory();
+  const firestore = useFirestore();
   const dispatch = useDispatch();
+  const currentUserId = firebase.auth().currentUser.uid
+
+  useFirestoreConnect([
+    { collection: 'restaurants' },
+    { collection: 'users' }
+  ]);
 
   const doSignOut = () => {
     firebase
       .auth()
       .signOut()
-      .then(function () {
+      .then(() => {
         const newCount = 0
         dispatch(a.userSignOut())
         dispatch(a.setCounter(newCount))
+        const propertiesToUpdate = {
+          matchedRestaurantArray: [],
+          currentFriend: null,
+          friendRestaurantArray: []
+        }
+        return (
+          firestore.update({
+            collection: 'users',
+            doc: currentUserId,
+          },
+            propertiesToUpdate
+          )
+        )
       })
       .then(history.push('/'))
-      .catch(function (error) {
+      .catch((error) => {
         console.log("Signout Error", error.message);
       });
   }
